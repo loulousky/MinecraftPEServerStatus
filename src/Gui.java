f293fd8670a3918b934cf0d41357277787f42584
tombailey <inbox@tombailey.me>
2014-05-25 00:49:52 +0100

    Improved GUI and a thread for networking

--- src/Gui.java
+++ src/Gui.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import javax.swing.*;
 
 import query.*;
@@ -16,27 +18,33 @@ public class Gui extends JFrame implements ActionListener {
 		this.setTitle("Minecraft Server status check");
 		this.setLocationRelativeTo(this.getRootPane());
 		this.setLayout(null);
-		//this.setResizable(false);
+		this.setResizable(false);
 		
 		JPanel content = new JPanel(null);
 		content.setBounds(0, 0, 220, 240);
 		
-		this.status = new JLabel("Unknown");
+		this.status = new JLabel("Unknown",SwingConstants.CENTER);
 		this.status.setFont(new Font("Oswald", Font.PLAIN, 24));
-		this.status.setBounds(20, 20, 160, 30);
+		this.status.setBounds(20, 20, 180, 30);
 		
+		JLabel hostnameLabel = new JLabel("Hostname:");
+		hostnameLabel.setBounds(20, 70, 60, 30);
 		this.hostname = new JTextField();
-		this.hostname.setBounds(20, 70, 160, 30);
+		this.hostname.setBounds(100, 70, 100, 30);
 		
-		this.hostport = new JTextField();
-		this.hostport.setBounds(20, 110, 160, 30);
+		JLabel hostportLabel = new JLabel("Port:");
+		hostportLabel.setBounds(20, 110, 60, 30);
+		this.hostport = new JTextField("19132");
+		this.hostport.setBounds(100, 110, 100, 30);
 		
 		JButton check = new JButton("Check status");
-		check.setBounds(40, 150, 120, 30);
+		check.setBounds(50, 150, 120, 30);
 		check.addActionListener(this);
 		
 		content.add(this.status);
+		content.add(hostnameLabel);
 		content.add(this.hostname);
+		content.add(hostportLabel);
 		content.add(this.hostport);
 		content.add(check);
 		this.getContentPane().add(content);
@@ -55,13 +63,18 @@ public class Gui extends JFrame implements ActionListener {
 		if (port == null) {
 			JOptionPane.showMessageDialog(null, "Port number should be an integer", "Invalid input", JOptionPane.ERROR_MESSAGE);
 		} else {
-			MCQuery query = new MCQuery(this.hostname.getText(), port);
-			try {
-				//System.out.println(query.stats().toString());
-				this.status.setText("Online");
-			} catch (NullPointerException npe) {
-				this.status.setText("Offline");
-			}
+			final Integer finalPort = port;
+			new Thread() {
+				public void run() {
+					MCQuery query = new MCQuery(hostname.getText(), finalPort);
+					try {
+						System.out.println(query.stats().toString());
+						status.setText("Online");
+					} catch (NullPointerException npe) {
+						status.setText("Offline");
+					}
+				}
+			}.start();
 		}
 	}
 }
